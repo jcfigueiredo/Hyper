d4bde6d0f98141e137db69d5a1394016afbc1c0a
Claudio Souza <jcfigueiredo@gmail.com>
2023-01-18 23:30:16 -0500

    Removes unnecessarily nested tests

--- test/hyper.test.ts
+++ test/hyper.test.ts
@@ -10,35 +10,33 @@ describe("Hypermedia", () => {
     await expect(Hypermedia.from(address)).rejects.toThrow("Invalid URL or endpoint: invalid-address");
   });
 
-  describe("Hypermedia", () => {
-    let hypermedia: Hypermedia;
-    const address = new URL("http://localhost/hypermedia");
+  let hypermedia: Hypermedia;
+  const address = new URL("http://localhost/hypermedia");
 
-    beforeEach(async () => {
-      hypermedia = await Hypermedia.from(address.href);
-    });
+  beforeEach(async () => {
+    hypermedia = await Hypermedia.from(address.href);
+  });
 
-    it("should return all links", () => {
-      const links = hypermedia.all();
-      expect(links).toEqual(links);
-    });
+  it("should return all links", () => {
+    const links = hypermedia.all();
+    expect(links).toEqual(links);
+  });
 
-    it("should return a specific link", () => {
-      const link = hypermedia.only("update");
-      expect(link).toEqual(links.find((link) => link.rel === "update"));
-    });
+  it("should return a specific link", () => {
+    const link = hypermedia.only("update");
+    expect(link).toEqual(links.find((link) => link.rel === "update"));
+  });
 
-    it("should throw error when trying to retrieve a non existing links", () => {
-      expect(() => hypermedia.only("not-found")).toThrow(new LinkNotFoundError("Link with rel 'not-found' not found"));
-    });
+  it("should throw error when trying to retrieve a non existing links", () => {
+    expect(() => hypermedia.only("not-found")).toThrow(new LinkNotFoundError("Link with rel 'not-found' not found"));
+  });
 
-    it("should follow the link and return the data", async () => {
-      const data = await hypermedia.follow<Hypermedia>("delete");
-      expect(data).toEqual({});
-    });
+  it("should follow the link and return the data", async () => {
+    const data = await hypermedia.follow<Hypermedia>("delete");
+    expect(data).toEqual({});
+  });
 
-    it("should throw an error if link not found when following a link", async () => {
-      await expect(hypermedia.follow("not-found")).rejects.toThrow(LinkNotFoundError);
-    });
+  it("should throw an error if link not found when following a link", async () => {
+    await expect(hypermedia.follow("not-found")).rejects.toThrow(LinkNotFoundError);
   });
 });
